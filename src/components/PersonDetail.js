a839937e724a3bbba768ccaa921b13154444f026
Admond tamang <admondtamang@gmail.com>
2020-08-07 21:09:39 +0545

    added table

--- src/components/PersonDetail.js
+++ src/components/PersonDetail.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import Grid from "@material-ui/core/Grid";
+import DeleteTwoToneIcon from "@material-ui/icons/DeleteTwoTone";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import EditIcon from '@material-ui/icons/Edit';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
-export default function PersonDetail() {
-    return (
-
-    )
+export default function PersonDetail({users}) {   
+    return       users.map((row) => (
+        <TableRow key={row.name}>
+          <TableCell component="th" scope="row">
+            {row.name}
+          </TableCell>
+          <TableCell align="right">{row.calories}</TableCell>
+          <TableCell align="right">{row.fat}</TableCell>
+          <TableCell align="right">{row.carbs}</TableCell>
+          <TableCell align="right">{row.protein}</TableCell>
+        </TableRow>
+      ))
+    
 }
--- src/pages/Teacherinfo.js
+++ src/pages/Teacherinfo.js
@@ -1,44 +1,109 @@
-import React, { useEffect, useState } from "react";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Layout from "../components/Layout";
+// import React, { useEffect, useState } from "react";
+// import { withStyles, makeStyles } from "@material-ui/core/styles";
+// import Table from "@material-ui/core/Table";
+// import TableBody from "@material-ui/core/TableBody";
+// import TableCell from "@material-ui/core/TableCell";
+// import TableContainer from "@material-ui/core/TableContainer";
+// import TableHead from "@material-ui/core/TableHead";
+// import TableRow from "@material-ui/core/TableRow";
+// import Paper from "@material-ui/core/Paper";
+// import Layout from "../components/Layout";
+// import Axios from "axios";
+// import PersonDetail from '../components/PersonDetail'
+
+// const StyledTableCell = withStyles((theme) => ({
+//   head: {
+//     backgroundColor: theme.palette.common.black,
+//     color: theme.palette.common.white,
+//   },
+//   body: {
+//     fontSize: 14,
+//   },
+// }))(TableCell);
+
+// const StyledTableRow = withStyles((theme) => ({
+//   root: {
+//     "&:nth-of-type(odd)": {
+//       backgroundColor: theme.palette.action.hover,
+//     },
+//   },
+// }))(TableRow);
+
+// const withStudentStyle = makeStyles({
+//   table: {
+//     minWidth: 700,
+//   },
+// });
+
+// export default function Teacherinfo() {
+//   const classes = withStudentStyle();
+//   const [users, setUsers] = useState([]);
+//   useEffect(() => {
+//     const userData = async () => {
+//       try {
+//         const { data } = await Axios.get(
+//           "http://localhost:30022/users/userteacher",
+//           {
+//             headers: {
+//               Authorization: `Bearer ${localStorage.getItem("token")}`,
+//             },
+//           }
+//         );
+//         setUsers(data);
+//         console.log("users :", data);
+//       } catch (e) {
+//         throw e;
+//       }
+//     };
+//     userData();
+//   }, []);
+//   return (
+//     <Layout>
+//       <TableContainer component={Paper}>
+//         <Table className={classes.table} aria-label="customized table">
+//           <TableHead>
+//             <TableRow>
+//               <StyledTableCell>Teacher</StyledTableCell>
+//               <StyledTableCell align="right">User Id</StyledTableCell>
+//               <StyledTableCell align="right">Classroom</StyledTableCell>
+//               <StyledTableCell align="right">Email</StyledTableCell>
+//               <StyledTableCell align="right">Phone</StyledTableCell>
+//               <StyledTableCell align="right">Actions</StyledTableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {users.map((row, index) => (
+//               <PersonDetail key={index} row StyledTableCell StyledTableRow/>
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </TableContainer>
+//     </Layout>
+//   );
+// }
+
+import React,{useState,useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+ import Layout from "../components/Layout";
 import Axios from "axios";
+import PersonDetail from '../components/PersonDetail'
 
-import Grid from "@material-ui/core/Grid";
-
-import DeleteTwoToneIcon from "@material-ui/icons/DeleteTwoTone";
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-const withStudentStyle = makeStyles({
+const useStyles = makeStyles({
   table: {
-    minWidth: 700,
+    minWidth: 650,
   },
 });
 
 export default function Teacherinfo() {
-  const classes = withStudentStyle();
+  const classes = useStyles();
   const [users, setUsers] = useState([]);
   useEffect(() => {
     const userData = async () => {
@@ -61,40 +126,23 @@ export default function Teacherinfo() {
   }, []);
   return (
     <Layout>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Teacher</StyledTableCell>
-              <StyledTableCell align="right">User Id</StyledTableCell>
-              <StyledTableCell align="right">Classroom</StyledTableCell>
-              <StyledTableCell align="right">Email</StyledTableCell>
-              <StyledTableCell align="right">Phone</StyledTableCell>
-              <StyledTableCell align="right">Actions</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map((row, index) => (
-              <StyledTableRow key={index}>
-                <StyledTableCell component="th" scope="row">
-                  {row.fname}
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.userid}</StyledTableCell>
-                <StyledTableCell align="right">
-                  {row.classroom.classroom} "{row.classroom.section}"
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.email}</StyledTableCell>
-                <StyledTableCell align="right">{row.phone}</StyledTableCell>
-                <StyledTableCell align="right">
-                  <Grid item xs={8}>
-                    <DeleteTwoToneIcon />
-                  </Grid>
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+              <TableCell>Teachers</TableCell>
+            <TableCell align="right">User Id</TableCell>
+            <TableCell align="right">Classroom</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Phone</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <PersonDetail users={users}/>
+        </TableBody>
+      </Table>
+    </TableContainer>
     </Layout>
   );
 }
